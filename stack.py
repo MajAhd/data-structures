ed18a06054318586dea548257dea914d81d0306e
Majid Ahmaditabar <mjd.ahd.tbr@gmail.com>
2021-10-05 22:12:45 +0330

    Stack

---
+++ stack.py
@@ -0,0 +1,32 @@
+from node import Node
+
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.height = new_node
+
+    def print_stack(self):
+        temp = self.top
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+
+    def pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None
+        self.height -= 1
+        return temp

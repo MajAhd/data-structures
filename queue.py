344d93fd68ba34c07dbfd4cd61c7d4d64bc47e64
Majid Ahmaditabar <mjd.ahd.tbr@gmail.com>
2021-10-05 22:13:36 +0330

    Stack/Queue

--- queue.py
+++ queue.py
@@ -24,18 +24,15 @@ class Queue:
             self.last = new_node
         self.length += 1
 
-    def dequeue(self, value):
+    def dequeue(self):
         if self.length == 0:
             return None
         temp = self.first
-        if self.length ==1:
+        if self.length == 1:
             self.first = None
             self.last = None
         else:
-            self.first =self.first.next
+            self.first = self.first.next
             temp.next = None
         self.length -= 1
         return temp
-
-my_queue = Queue(4)
-my_queue.print_queue()
